ffb1e248aa432695caf485f9d0739ce8318a5e6e
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-05-26 23:58:12 +0600

    1.0.2

---
+++ sidebar-author.php
@@ -0,0 +1,22 @@
+<div class="widget-area secondary" id="secondary" role="complementary" itemtype="https://schema.org/WPSideBar" itemscope="itemscope">
+    <div class="sidebar-main">
+        <aside id="nav_menu-3" class="widget widget_nav_menu">
+            <h2 class="widget-title">Categories</h2>
+            <nav class="menu-sidebar-menu-container" aria-label="Categories">
+                <ul id="menu-sidebar-menu" class="menu">
+                    <li id="menu-item-13569" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13569"><a href="https://designil.activebd4u.com/category/design/" class="menu-link">Design</a></li>
+                    <li id="menu-item-13570" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13570"><a href="https://designil.activebd4u.com/category/recommend/" class="menu-link">Recommended</a></li>
+                    <li id="menu-item-13571" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13571"><a href="https://designil.activebd4u.com/category/tutorials/" class="menu-link">Tutorials</a></li>
+                    <li id="menu-item-13572" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13572"><a href="https://designil.activebd4u.com/category/coding/" class="menu-link">Coding</a></li>
+                    <li id="menu-item-13573" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13573"><a href="https://designil.activebd4u.com/category/ux-design/" class="menu-link">UX Design</a></li>
+                    <li id="menu-item-13574" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13574"><a href="https://designil.activebd4u.com/category/others/" class="menu-link">Others</a></li>
+                    <li id="menu-item-13575" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13575"><a href="https://designil.activebd4u.com/category/downloads/" class="menu-link">Downloads</a></li>
+                    <li id="menu-item-13576" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13576"><a href="https://designil.activebd4u.com/category/web-design/" class="menu-link">Web Design</a></li>
+                    <li id="menu-item-13577" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13577"><a href="https://designil.activebd4u.com/category/ui-design/" class="menu-link">UI Design</a></li>
+                    <li id="menu-item-13578" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13578"><a href="https://designil.activebd4u.com/category/css3/" class="menu-link">CSS3</a></li>
+                </ul>
+            </nav>
+        </aside>
+        <aside id="media_image-2" class="widget widget_media_image"><img width="500" height="625" src="https://designil.activebd4u.com/wp-content/uploads/2021/05/6082d0f997bf4a5906af90d9_banner-sidebar-p-500.jpeg" class="image wp-image-13520  attachment-full size-full" alt="" loading="lazy" style="max-width: 100%; height: auto;" srcset="https://designil.activebd4u.com/wp-content/uploads/2021/05/6082d0f997bf4a5906af90d9_banner-sidebar-p-500.jpeg 500w, https://designil.activebd4u.com/wp-content/uploads/2021/05/6082d0f997bf4a5906af90d9_banner-sidebar-p-500-240x300.jpeg 240w" sizes="(max-width: 500px) 100vw, 500px"></aside>
+    </div><!-- .sidebar-main -->
+</div>
