534c56ce7b55372bef4d7c4bd6bbc5ce0f8f4177
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-05-28 00:00:37 +0600

    Update sidebar-author.php

--- sidebar-author.php
+++ sidebar-author.php
@@ -10,13 +10,13 @@
             <?php echo wp_get_attachment_image ($media, 'full', false, array('class' => 'rounded-3 mb-20'));?>
             <h4 class="team-member-name mb-10"><?php echo get_the_author_meta('display_name') ?></h4>
             <div class="team-member-bio mb-20"><?php echo $description; ?></div>
-            <?php if ($social_links) : ?>
+            
                 <ul class="list-inline">
                     <?php foreach ( $social_links as $link ) : ?>
                         <li><?php echo $link; ?></li>
                     <?php endforeach;?>
                 </ul>
-            <?php endif;?>
+            
         </aside>
         <aside id="media_image-2" class="widget widget_media_image"><img width="500" height="625" src="https://designil.activebd4u.com/wp-content/uploads/2021/05/6082d0f997bf4a5906af90d9_banner-sidebar-p-500.jpeg" class="image wp-image-13520  attachment-full size-full" alt="" loading="lazy" style="max-width: 100%; height: auto;" srcset="https://designil.activebd4u.com/wp-content/uploads/2021/05/6082d0f997bf4a5906af90d9_banner-sidebar-p-500.jpeg 500w, https://designil.activebd4u.com/wp-content/uploads/2021/05/6082d0f997bf4a5906af90d9_banner-sidebar-p-500-240x300.jpeg 240w" sizes="(max-width: 500px) 100vw, 500px"></aside>
     </div><!-- .sidebar-main -->
